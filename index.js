25ffb90e912313f27e5674bc75d6ae3b201d0943
Abhay_node <abhay.jadav@sensussoft.com>
2024-04-03 10:07:16 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+
+
+const freecurrencyapi = new Freecurrencyapi('fca_live_CsB5hILgq1oHtp7mXbyNgSbWgPyPbtzaWjKaIW1J');
+
+convertCurrency("INR","USD",3);
+
+export async function convertCurrency(fromCurrency,toCurrency,units){
+const res = await freecurrencyapi.latest({
+        base_currency: fromCurrency,
+        currencies: toCurrency
+    })
+    const multiplier = res.data[toCurrency]
+    // console.log(multiplier);
+    return multiplier*units;
+}
